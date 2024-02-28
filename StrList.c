b63b153dfbc32b6f08be7ed77d6ad749a93c2a25
Amit Gini <Amitgini0369@gmail.com>
2024-02-28 13:02:38 +0200

    Update

--- StrList.c
+++ StrList.c
@@ -15,7 +15,7 @@ struct _StrList {
     size_t _size;
 };
 
-void free_Node(Node *pNode);
+void free_Node(Node *Node);
 
 // Allocation for the node and its data
 Node *Node_alloc(const char *data, Node *next) {
@@ -24,63 +24,62 @@ Node *Node_alloc(const char *data, Node *next) {
         return NULL;
     }
 
-    Node *pNode = (Node*)malloc(sizeof(Node)); // Memory allocation for the node
+    Node *ptrNode = (Node*)malloc(sizeof(Node)); // Memory allocation for the node
 
-    if (pNode == NULL) {// Check Failure to allocate memory
+    if (ptrNode == NULL) {// Check Failure to allocate memory
         fprintf(stderr, "Error, Allocation memory for new node failed\n");
-        free(pNode);
+        free(ptrNode);
         return NULL;
     }
 
-    char* pData = (char*)malloc(strlen(data) + 1);  // Memory allocation for the data field of node
+    char* ptrData = (char*)malloc(strlen(data) + 1);  // Memory allocation for the data field of node
 
-    if(pData == NULL){
+    if(ptrData == NULL){
         fprintf(stderr, "Error allocating memory for node data\n" );
-        free_Node(pNode);  // node and data field of node freed
+        free_Node(ptrNode);  // node and data field of node freed
         return NULL;
     }
 
-    strcpy(pData, data);  // copying the data to the node field
-    pNode -> _data = pData;  // pointer to the data of the node
-    pNode -> _next = next;  //sets the next node
-    return pNode;
+    strcpy(ptrData, data);  // copying the data to the node field
+    ptrNode -> _data = ptrData;  // pointer to the data of the node
+    ptrNode -> _next = next;  //sets the next node
+    return ptrNode;
 }
 
 //Free the memory that been allocated for the node
 //todo: need to be tested
-void free_Node(Node *pNode){
-    if(pNode != NULL)
-        free(pNode -> _data);
-    free(pNode);
+void free_Node(Node *node){
+    if(node != NULL)
+        free(node -> _data);
+    free(node);
 }
 
 StrList* StrList_alloc(){
-    StrList *pStrList = (StrList*)malloc(sizeof(StrList));
+    StrList *ptrStrList = (StrList*)malloc(sizeof(StrList));
 
-    if(pStrList == NULL){
+    if(ptrStrList == NULL){
         fprintf(stderr, "Error allocation memory for string list\n");
-        StrList_free(pStrList);
+        StrList_free(ptrStrList);
         return NULL;
     }
 
-    pStrList -> _head = NULL;
-    pStrList -> _size = 0;
-    return pStrList;
+    ptrStrList -> _head = NULL;
+    ptrStrList -> _size = 0;
+    return ptrStrList;
 }
+
 //todo: need to be tested
 void StrList_free(StrList* strList){
     if (strList != NULL) {
+        Node *ptrNode = strList->_head;
+        Node *ptrTempNode;
 
-        Node *pNode = strList->_head;
-        Node *pTempNode;
-
-        while (pNode != NULL) {
-            pTempNode = pNode;
-            pNode = pNode->_next;
-            free_Node(pTempNode);
+        while (ptrNode != NULL) {
+            ptrTempNode = ptrNode;
+            ptrNode = ptrNode->_next;
+            free_Node(ptrTempNode);
         }
     }
-
     free(strList);
 }
 
@@ -102,11 +101,9 @@ void StrList_insertLast(StrList* StrList, const char* data) {
         StrList->_head = ptrNewNode;
 
     } else {
-
         while (ptrNode->_next != NULL) {  // next to the last node, and insert after it,
             ptrNode = ptrNode->_next;
         }
-
         ptrNode -> _next = ptrNewNode;
     }
 
@@ -119,43 +116,37 @@ void StrList_insertAt(StrList* StrList, const char* data, int index) {
         printf("Invalid Index\n");
         return;
     }
-
     else if(data == NULL || strlen(data) < 1){
         printf("Invalid data\n");
         return;
-    }
 
-    else {
-        Node* pNode = StrList -> _head;
-        Node* pNewNode = Node_alloc(data, NULL);
+    } else {
+        Node* ptrNode = StrList -> _head;
+        Node* ptrNewNode = Node_alloc(data, NULL);
 
-        if(pNewNode == NULL) return;   // Case 3: Node_allocation failed, Null exception handled in the alloc_Node function
+        if(ptrNewNode == NULL) return;   // Case 1: Node_allocation failed, Null exception handled in the alloc_Node function
 
         if(index == 0) {
-            pNewNode -> _next = pNode;
-            StrList -> _head = pNewNode;
-        }
+            ptrNewNode -> _next = ptrNode;
+            StrList -> _head = ptrNewNode;
 
-        else if(pNode == NULL) {  // Case 4: pNode is the _head, equal to Null, and index is larger than 0, Fail and exit.
+        } else if(ptrNode == NULL) {  // Case 2: ptrNode is the _head, equal to Null, and index is larger than 0, Fail and exit.
             printf("Head is Null, insertion at index larger then 0 Failed");
             return;
-        }
 
-        else {
+        } else {
             int nodeAtIndex = 1;
 
             while(nodeAtIndex != index) {
-                if (pNode == NULL) {
+                if (ptrNode == NULL) {
                     printf("Invalid index");
                     return;
                 }
-
-                pNode = pNode->_next;
+                ptrNode = ptrNode->_next;
                 nodeAtIndex++;
             }
-
-            pNewNode -> _next = pNode ->_next;
-            pNode -> _next = pNewNode;
+            ptrNewNode -> _next = ptrNode ->_next;
+            ptrNode -> _next = ptrNewNode;
         }
 
         StrList -> _size++;
@@ -205,8 +196,8 @@ void StrList_printAt(const StrList* Strlist,int index){
     if(Strlist == NULL){
         printf("StrList is NULL\n");
         return;
-    }
-    else if(Strlist -> _size == 0){
+
+    } else if(Strlist -> _size == 0){
         printf("StrList is empty\n");
         return;
     }
@@ -218,82 +209,78 @@ void StrList_printAt(const StrList* Strlist,int index){
         return;
     }
 
-    Node* pNode = Strlist -> _head;
+    Node* ptrNode = Strlist -> _head;
     int nodeAtIndex = 0;
 
     while(nodeAtIndex != index){
-        if(pNode -> _next == NULL){
+        if(ptrNode -> _next == NULL){
             printf("There is no data in that index\n");
             return;
         }
-
-        pNode = pNode -> _next;
+        ptrNode = ptrNode -> _next;
         nodeAtIndex++;
     }
 
-    printf("%s\n",pNode -> _data);
+    printf("%s\n", ptrNode -> _data);
 }
 
-//
-//int StrList_printLen(const StrList* Strlist){
-//    size_t sizeStrList = Strlist -> _size;
-//    if(sizeStrList == 0) return 0;
-//
-//    Node* pNode = Strlist -> _head;
-//    if(pNode == NULL) return 0;
-//
-//    if(pNode -> _data == NULL) return 0;
-//
-//    size_t charCounter = 0;
-//    char* tempNodeData;
-//
-//    while(pNode != NULL){
-//
-//        tempNodeData = pNode -> _data;
-//        if(tempNodeData != NULL) charCounter += strlen(tempNodeData);
-//
-//        pNode = pNode -> _next;
-//    }
-//
-//    if(charCounter > INT_MAX) {
-//        fprintf(stderr,"Number of char is greater then INT_MAX, return Failed!");
-//        return -1;
-//    }
-//
-//    return (int)charCounter;
-//}
-//
-//int StrList_count(StrList* StrList, const char* data){
-//    size_t sizeStrList = StrList -> _size;
-//    if(sizeStrList == 0) return 0;
-//
-//    Node* pNode = StrList -> _head;
-//    if(pNode == NULL) return 0;
-//
-//    size_t counterSubData = 0;
-//    char* pTempData = pNode -> _data;
-//    size_t sizeTempData;  // data length in the node.
-//    size_t sizeSubData= strlen(data);  // data length of the given argument
-//
-//    while(pNode != NULL) {
-//
-//        if(pTempData != NULL) {
-//            sizeTempData = strlen(pTempData);
-//
-//            for (int i = 0; i <= sizeTempData - sizeSubData; i++) {
-//                if (strncmp(&pTempData[i], data, sizeSubData) ==
-//                    0) {  // Compare substrings of data in pTempData, start for index i
-//                    counterSubData++;
-//                }
-//            }
-//        }
-//
-//        pNode = pNode -> _next;
-//    }
-//
-//    if(counterSubData > INT_MAX) {
-//        fprintf(stderr,"Number of char is greater then INT_MAX, return Failed!");
-//        return -1;
-//    }
-//    return (int)counterSubData;
-//}
+
+int StrList_printLen(const StrList* Strlist){
+    if(Strlist == NULL) return 0;
+
+    size_t sizeStrList = Strlist -> _size;
+    if(sizeStrList == 0) return 0;
+
+    Node* ptrNode = Strlist -> _head;
+    if(ptrNode == NULL) return 0;
+
+    size_t charCounter = 0;
+    char* tempNodeData;
+
+    while(ptrNode != NULL){
+
+        tempNodeData = ptrNode -> _data;
+        if(tempNodeData != NULL) charCounter += strlen(tempNodeData);
+
+        ptrNode = ptrNode -> _next;
+    }
+
+    if(charCounter > INT_MAX) {
+        fprintf(stderr,"Number of char is greater then INT_MAX, return Failed!");
+        return -1;
+    }
+
+    return (int)charCounter;
+}
+
+int StrList_count(StrList* StrList, const char* data){
+    if(StrList == NULL) return 0;
+
+    size_t sizeStrList = StrList -> _size;
+    if(sizeStrList == 0) return 0;
+
+    Node* ptrNode = StrList -> _head;
+    char* ptrWord = ptrNode -> _data;
+    const size_t sizeArgWord = strlen(data);  // data length of the given argument
+    size_t sizeWordInList = 0;  // data length in the node - dynamic.
+    size_t counterSubWord = 0;
+
+    while(ptrNode != NULL) {
+
+        if(ptrWord != NULL) {
+            sizeWordInList = strlen(ptrWord);
+
+            for (int i = 0; i <= sizeWordInList - sizeArgWord; i++) {
+                
+//                if (strncmp(&ptrWord[i], data, sizeArgWord) == 0) counterSubWord++;  // Compare substrings of data in ptrWord, start for index i
+            }
+        }
+        ptrNode = ptrNode -> _next;
+    }
+
+    if(counterSubWord > INT_MAX) {
+        fprintf(stderr,"Number of char is greater then INT_MAX, return Failed!");
+        return -1;
+    }
+    return (int)counterSubWord;
+}
--- main.c
+++ main.c
@@ -11,22 +11,21 @@ int main(){
         return -1;
     }
 
-    int input = -1; // default
-    while(input != 0) {
-        scanf("%d",&input);
+    int inputCase = -1; // default
+    while(inputCase != 0) {
+        scanf("%d",&inputCase);
 
         char *string1 = {"Hello"};
         char *string2 = {"is"};
         char *string3 = {"it"};
         char *string4 = {"working?"};
-        char *string5 = {"TestCase2?"};
+        char *string5 = {"Test-wrwrwwrw?"};
 
-        switch (input) {
+        switch (inputCase) {
 
-            case 1:
+            case 1:  // insert number of words and then the words to insert in the last position
                 //todo: Implementation - for now Just tests
                 printf("Enter number of words: 4\n");
-                printf("Hello is it working?\n");
 
                 StrList_insertLast(ptrStrList, string1);
                 StrList_insertLast(ptrStrList, string2);
@@ -35,26 +34,29 @@ int main(){
 
                 break;
 
-            case 2:
+            case 2:  // insert word at specific index
                 //todo: In case double is being send, it covert to int and still work, check required limitations.
                 StrList_insertAt(ptrStrList, string5,1);
                 break;
 
-            case 3:
+            case 3:  // printing all the words
                 StrList_print(ptrStrList);
                 break;
 
-            case 4:
+            case 4:  // printing the number of words
                 printf("%zu\n", StrList_size(ptrStrList));
                 break;
 
-            case 5:
+            case 5:  // printing word in a specific index
                 StrList_printAt(ptrStrList,4);
                 break;
-            case 6:
+
+            case 6:  // printing the number of chars in the list
+                printf("%d\n",StrList_printLen(ptrStrList));
                 break;
 
             case 7:
+                printf("%d\n", StrList_count(ptrStrList,"wrw"));
                 break;
 
             case 8:
@@ -79,7 +81,7 @@ int main(){
                 break;
 
             default:
-                printf("%d Invalid Case, try again 0 - 13", input);
+                printf("%d Invalid Case, try again 0 - 13", inputCase);
         }
     }
 
